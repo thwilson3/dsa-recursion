06164c00caebd689fe4a43678c1a39bf11d67519
Alyssa Denary <alyssa.denary@gmail.com>
2023-05-30 18:49:37 -0400

    add recursion probs

--- recursion.js
+++ recursion.js
@@ -9,7 +9,7 @@ function product(nums) {
 /** longest: return the length of the longest word in an array of words. */
 
 function longest(words) {
-  console.log(words);
+  // console.log(words);
   if (words.length === 0) return 0;
 
   // if (words[0]?.length > words[1]?.length){
@@ -37,23 +37,52 @@ function longest(words) {
 
 /** everyOther: return a string with every other letter. */
 
-function everyOther(str) {}
+function everyOther(str) {
+  if (str.length === 0) return "";
+
+  return str[0] + everyOther(str.slice(2));
+}
 
 /** find: return boolean depending on if val exists in array or not. */
 
-function find(arr, val) {}
+function find(arr, val) {
+  // base case arr.length === 0 return false
+  if (arr.length === 0) return false;
+
+  if (arr[0] === val) return true;
+
+  return find(arr.slice(1), val);
+}
 
 /** isPalindrome: checks whether a string is a palindrome or not. */
 
-function isPalindrome(str) {}
+function isPalindrome(str) {
+  // if 1 letter, true, if no letters, true
+  if (str.length <= 1) return true;
+
+  // get first and last section and check if they don't match, return false
+  if (str[0] !== str[str.length - 1]) return false;
+
+  // recursively past
+  return isPalindrome(str.slice(1, -1));
+}
 
 /** revString: return a copy of a string, but in reverse. */
 
-function revString(str) {}
+function revString(str) {
+  if (str.length === 0) return "";
+  return revString((str.slice(1))) + str[0];
+}
 
 /** findIndex: return the index of val in arr (or -1 if val is not present). */
 
-function findIndex(arr, val) {}
+function findIndex(arr, val, idx=0) {
+  if (arr.length === idx) return -1;
+
+  if (arr[idx] === val) return idx;
+
+  return findIndex(arr, val, ++idx);
+}
 
 /** gatherStrings: given an object, return an array of all of the string values. */
 
